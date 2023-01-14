e3fd077146e59b80e2548dddb7c2d31a1f481f68
“MohanH99” <mohanhansda2@gmail.com>
2023-01-14 13:06:55 +0530

    Invoking Overridden

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -13,6 +13,25 @@ class A{
 }
 class B extends A{
     void meth(){
+        super.meth();
         System.out.println("Method is overridden in Extendend class B");
+    }
+}
+
+
+
+
+class Vehicle{
+    //defining a method
+    void run(){System.out.println("Vehicle is running");}
+}
+//Creating a child class
+class Bike2 extends Vehicle{
+    //defining the same method as in the parent class
+    void run(){System.out.println("Bike is running safely");}
+
+    public static void main(String args[]){
+        Bike2 obj = new Bike2();//creating object
+        obj.run();//calling method
     }
 }
